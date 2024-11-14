4d8f59887df521c46ee0e7bbafac9c61d334f030
atiets <nguyenanhtuyet03.nbk@gmail.com>
2024-11-14 12:28:23 +0700

    authorization

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import Login from './components/Login/Login';
 import Header from './components/User/Header/Header';
 import Home from './components/User/Home/Home';
 import Footer from './components/Footer/Footer';
+import AdminHeader from './components/Admin/AdminHeader/AdminHeader';
+import AdminDashboard from './components/Admin/AdminDashboard/AdminDashboard'
 import './App.css';
 
 function App() {
@@ -13,6 +15,15 @@ function App() {
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+
+          <Route path="/admin-dashboard" element={
+          <>
+            <AdminHeader /> 
+            <AdminDashboard /> 
+            <Footer /> 
+          </>
+        } />
+
           <Route path="/" element={
             <div className="content-wrapper">
               <Header />
